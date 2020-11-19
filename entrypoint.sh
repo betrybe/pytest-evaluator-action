a3c6598376d1eed5653f005f2cf72622a1338de5
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2020-11-19 15:22:29 -0300

    Parar execução do entrypoint em caso de erros

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 set -x
 
-output=$(git clone --single-branch --branch "$INPUT_REPOSITORY_MAIN_BRANCH" "https://github.com/$GITHUB_REPOSITORY.git" /github/main-branch/ 2>&1)
-if [[ "$output" == *"Could not find remote branch"* ]]; then
-  printf "Given main branch $INPUT_REPOSITORY_MAIN_BRANCH not found\n"
+git clone --single-branch --branch "$INPUT_REPOSITORY_MAIN_BRANCH" "https://github.com/$GITHUB_REPOSITORY.git" /github/main-branch/
+if [ $? != 0 ]; then
+  printf "Execution error $?"
   exit 1
 fi
 
@@ -13,12 +13,17 @@ cp -r /github/main-branch/tests /github/workspace
 
 # Install deps and run pytest over the student source
 cd /github/workspace
-if test -f "dev-requirements.txt"; then
+if test -f "dev-requirements.txt" ; then
   python3 -m pip install -r dev-requirements.txt
 else
   python3 -m pip install -r requirements.txt
 fi
 python3 -m pytest --json=/tmp/report.json
+
+if [ $? != 0 ]; then
+  printf "Execution error $?"
+  exit 1
+fi
 
 # Run evaluator over pytest result assuring that the requirements file is the original
 python3 /home/evaluation.py /tmp/report.json /github/main-branch/.trybe/requirements.json > /tmp/evaluation_result.json
