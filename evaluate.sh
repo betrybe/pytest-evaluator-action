98ed07cb25582bf8feb937b245a96b4819d12055
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2022-01-11 10:12:01 -0300

    fix: usa venv para instalar dependências

--- evaluate.sh
+++ evaluate.sh
@@ -1,9 +1,26 @@
-#!/bin/sh -l
+#!/bin/bash
+
+python3 -m pip install virtualenv wheel --no-cache-dir
 
 # Install deps and run pytest over the student source
+python3 -m venv "~/$INPUT_PR_AUTHOR_USERNAME-project" --system-site-packages
+source "~/$INPUT_PR_AUTHOR_USERNAME-project/bin/activate"
 if test -f "dev-requirements.txt" ; then
-  python3 -m pip install -r dev-requirements.txt
+  python3 -m pip install -r dev-requirements.txt --no-cache-dir
 else
-  python3 -m pip install -r requirements.txt
+  python3 -m pip install -r requirements.txt --no-cache-dir
 fi
 python3 -m pytest --json=/tmp/report.json
+
+python3 -m venv "~/$INPUT_PR_AUTHOR_USERNAME-evaluator" --system-site-packages
+source "~/$INPUT_PR_AUTHOR_USERNAME-evaluator/bin/activate"
+python3 -m pip install -r "$EVALUATOR_REQUIREMENTS"
+python3 "$EVALUATOR_SRC/evaluation.py" /tmp/report.json .trybe/requirements.json > /tmp/evaluation_result.json
+
+if [ $? != 0 ]; then
+  printf "Execution error $?"
+  exit 1
+fi
+
+echo `cat /tmp/evaluation_result.json | base64 -w 0`
+echo ::set-output name=result::`cat /tmp/evaluation_result.json | base64 -w 0`
